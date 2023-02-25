e6934597c835534561daea542a50d835de702643
Ethan Flute <ethansamf@gmail.com>
2023-02-25 14:32:56 -0600

    Add routing.

--- app/src/Navbar.tsx
+++ app/src/Navbar.tsx
@@ -3,22 +3,26 @@ import { Link } from "react-router-dom";
 
 function Navbar() {
     return (
-        <nav>
-            <ul>
-                <li>
-                    <Link to="/">Landing</Link>
-                </li>
-                <li>
-                    <Link to="/applications">Applications</Link>
-                </li>
-                <li>
-                    <Link to="/opportunities">Opportunities</Link>
-                </li>
-                <li>
-                    <Link to="/aboutus">AboutUs</Link>
-                </li>
-            </ul>
-        </nav>
+        <div className="Navbar">
+            <header className="Navbar-header">
+                <nav>
+                    <ul>
+                        <li>
+                            <Link to="/">Landing</Link>
+                        </li>
+                        <li>
+                            <Link to="/applications">Applications</Link>
+                        </li>
+                        <li>
+                            <Link to="/opportunities">Opportunities</Link>
+                        </li>
+                        <li>
+                            <Link to="/aboutus">AboutUs</Link>
+                        </li>
+                    </ul>
+                </nav>
+            </header>
+        </div>
     );
 }
 

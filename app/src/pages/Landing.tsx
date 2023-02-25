f7ad0293a17cc70e6c6044ceacbb8db1e6bddaed
Ethan Flute <ethansamf@gmail.com>
2023-02-25 15:57:02 -0600

    Change landing page message.

--- app/src/pages/Landing.tsx
+++ app/src/pages/Landing.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 function Landing() {
-    return <h1>this is the landing page</h1>;
+    return <h1>this is the landing page but new</h1>;
 }
 
 export default Landing;

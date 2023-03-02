11890c1e56399eb25fa68778104215d68f05e62c
kasiyo <kaiasiripanyo@gmail.com>
2023-03-02 15:52:31 -0600

    added HoverButton component, minor Landing page edit
    - HoverButton component accepts optional onClick, text, color, and hoverColor parameters
    
    - added <div> tag to Landing page so component could be tested

---
+++ app/src/components/HoverButton.tsx
@@ -0,0 +1,49 @@
+import React, { useState } from "react";
+
+/*
+A button component that takes in optional props and changes color when
+hovered upon
+
+EXAMPLE USAGE:
+    <HoverButton 
+        onClick={event/element}
+        text="henlo"
+        color="rgb(120, 196, 234)"
+        hoverColor="rgb(124, 172, 203)"
+    />
+ */
+
+interface Props {
+    onClick?: () => void;
+    text?: string,
+    color?: string,
+    hoverColor?: string
+}
+
+const HoverButton: React.FC<Props> = ({onClick, text, color, hoverColor}) => {
+    const [isHovered, setIsHovered] = useState(false);
+
+    const style = { 
+        backgroundColor: isHovered && hoverColor ? hoverColor : color || 'transparent',
+        border: 'none',
+    };
+
+    const handleClick = () => {
+        console.log('button click!');
+    };
+
+    return (
+        <div className="HoverButton">
+            <button 
+                onClick={ onClick ? onClick : handleClick}
+                style={style}
+                onMouseEnter={() => setIsHovered(true)}
+                onMouseLeave={() => setIsHovered(false)}
+            >
+                {text}
+            </button>
+        </div>
+    );
+}
+
+export default HoverButton;
--- app/src/pages/Landing.tsx
+++ app/src/pages/Landing.tsx
@@ -1,7 +1,12 @@
 import React from "react";
+import HoverButton from "../components/HoverButton";
 
 function Landing() {
-    return <h1>this is the landing page</h1>;
+    return (
+        <div>
+            <h1>this is the landing page</h1>
+        </div>
+    );
 }
 
 export default Landing;

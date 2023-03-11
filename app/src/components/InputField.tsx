e0ff7cd70af4cc7ba2a661e6c50f25d8c08e8869
Farooq, Salman <salmanfarooq@my.unt.edu>
2023-03-11 12:40:05 -0600

    Added InputField component to project

---
+++ app/src/components/InputField.tsx
@@ -0,0 +1,14 @@
+import React, { useState } from "react";
+
+type Props = {
+	text?: string;
+
+};
+
+const InputField = ({text}: Props) => {
+	return (
+		<div>InputField</div>
+	)
+};
+
+export default InputField;

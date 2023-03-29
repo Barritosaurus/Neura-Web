c28c2de49dfe6a93768571e603f3a966dbaa0867
kasiyo <kaiasiripanyo@gmail.com>
2023-03-29 01:39:36 -0500

    added Shadow HOC, updated HoverButton
    - added shadow higher-order component that casts a shadow behind a given component
    - updated some hoverbutton styling

--- app/src/components/Shadow.tsx
+++ app/src/components/Shadow.tsx
@@ -1,31 +1,46 @@
-import React, { useState, useEffect, forwardRef } from "react";
+/*
+A shadow HOC that takes in optional props and casts a shadow behind a given component
 
-interface ShadowProps {
-    shadowColor?: string,
-    shadowOffset?: {x: number; y: number },
-    shadowOpacity?: number,
-    shadowRadius?: number
+EXAMPLE USAGE:
+    <Shadow>
+        <{component}/>
+    </Shadow>
+*/
+
+import React from "react";
+
+var values = {
+    offsetX: 2,
+    offsetY: 2,
+    blur: 2,
+    spread: 0.25,
+    color: '#5A5A5A',
+    padding: 5,
+    borderRadius: 5,
 }
 
-const shadow = <P extends object>(Component: React.ComponentType<P>, {
-    shadowColor = '#000000',
-    shadowOffset = {x: 0, y: 2},
-    shadowOpacity = 0.2,
-    shadowRadius = 2,
-}: ShadowProps = {}) => {
-    const Shadow: React.FC<P> = (props) => {
-        
-        const shadow = {
-            shadowColor,
-            shadowOffset,
-            shadowOpacity,
-            shadowRadius,
-            elevation: shadowRadius
-        };
-
-        return <Component {...(props as P)} style={[props, shadow]} />;
-    };
-    return Shadow;
+type ShadowProps = {
+    offsetX?: number;
+    offsetY?: number;
+    blur?: number;
+    spread?: number;
+    color?: string;
+    children: React.ReactElement;
+    borderRadius?: number,
 }
 
-export default shadow;
+const Shadow = (props: ShadowProps) => {
+    var shadowStyle = {
+        boxShadow: `${props.offsetX || values.offsetX}px ${props.offsetY || values.offsetY}px ${props.blur || values.blur}px ${props.spread || values.spread}px ${props.color || values.color}`,
+        display: "flex",
+        borderRadius: `${props.borderRadius || values.borderRadius}px`,
+    }
+    
+    return (
+        <div style={shadowStyle}>
+            {props.children}
+        </div>
+    )
+} 
+
+export default Shadow;
--- app/src/pages/Landing.tsx
+++ app/src/pages/Landing.tsx
@@ -1,5 +1,48 @@
-import React from "react";
+import React, { Component } from "react";
 import { Link } from "react-router-dom";
+import shadow from "../components/Shadow";
+import HoverButton from "../components/HoverButton";
+import AboutUs from "./AboutUs";
+
+//const ShadowHoverButton = shadow(HoverButton);
+
+/*function ShadedButton() {
+
+    const addChangeListener = () => {
+        console.log("add change listener!")
+    }
+    const removeChangeListener = () => {
+        console.log("remove change listener!")
+    }
+
+    return class extends Component {
+        constructor(props: any) {
+            super(props);
+            this.handleChange = this.handleChange.bind(this);
+            this.state = {
+                data: selectData(WrappedComponent, props)
+            };
+        }
+
+        componentDidMount() {
+            addChangeListener.bind(this.handleChange);
+        }
+        componentWillUnmount() {
+            removeChangeListener.bind(this.handleChange);
+        }
+
+        handleChange() {
+            this.setState({
+                data: selectData(WrappedComponent, this.props)
+            })
+        }
+
+        render() {
+            return <Component data={this.state} {...this.props} />;
+        }
+    }
+}*/
+
 function Landing() {
     return (
         <div className="background">
@@ -21,8 +64,10 @@ function Landing() {
                         </li>
                     </header>
                 </div>
+                
             </div>
             <div className="grid-container">
+                
                 <div className="image-grid">
                     <div className="image">
                         <img src="" alt="" />

98d30c940fd094059505ed9fd333972db622b268
kasiyo <kaiasiripanyo@gmail.com>
2023-03-30 18:22:23 -0500

    Update Landing.tsx
    forgot to remove unused code

--- app/src/pages/Landing.tsx
+++ app/src/pages/Landing.tsx
@@ -1,47 +1,5 @@
-import React, { Component } from "react";
+import React from "react";
 import { Link } from "react-router-dom";
-import shadow from "../components/Shadow";
-import HoverButton from "../components/HoverButton";
-import AboutUs from "./AboutUs";
-
-//const ShadowHoverButton = shadow(HoverButton);
-
-/*function ShadedButton() {
-
-    const addChangeListener = () => {
-        console.log("add change listener!")
-    }
-    const removeChangeListener = () => {
-        console.log("remove change listener!")
-    }
-
-    return class extends Component {
-        constructor(props: any) {
-            super(props);
-            this.handleChange = this.handleChange.bind(this);
-            this.state = {
-                data: selectData(WrappedComponent, props)
-            };
-        }
-
-        componentDidMount() {
-            addChangeListener.bind(this.handleChange);
-        }
-        componentWillUnmount() {
-            removeChangeListener.bind(this.handleChange);
-        }
-
-        handleChange() {
-            this.setState({
-                data: selectData(WrappedComponent, this.props)
-            })
-        }
-
-        render() {
-            return <Component data={this.state} {...this.props} />;
-        }
-    }
-}*/
 
 function Landing() {
     return (
